3410510dad8a343a58b5b137199265afdee54d6a
otabek231 <otabek.pro@hotmail.com>
2023-09-18 14:20:39 +0500

    Axios request to server

--- EasyBankReactApp/src/App.jsx
+++ EasyBankReactApp/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css' 
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const req = {
+    employeeId: "3103e37f-3b9c-4b91-8ac9-3b9672ee5eda",
+    startDate: "2023-09-01",
+    endDate: "2023-09-19",
+  };
+
+  const [startDate, setStartDate] = useState("2023-01-01");
+  const [endDate, setEndDate] = useState("2023-01-01");
+
+ useEffect(() => {
+  console.log(startDate);
+  console.log(endDate);
+  
+  req.startDate = startDate;
+  req.endDate = endDate;
+   axios
+     .post("https://localhost:7042/api/report", req)
+     .then((response) => {
+       console.log(response.data.data);
+     })
+     .catch((error) => {
+       console.log(error);
+     });
+ }, [startDate, endDate]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=''>
+      <h3>Start data</h3>
+      <input type="date" onChange={(e) => setStartDate(e.target.value)} />
+      <h3>End data</h3>
+      <input type="date" onChange={(e) => setEndDate(e.target.value)} />
+    </div>
   );
 }
 
